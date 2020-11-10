9bdfa0ccc391931674e1978423c2e487f28123d8
Razor _Z_ Pi <popovp.popoff2001@gmail.com>
2020-11-10 22:04:00 +0700

    Lesson_3 Class

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,19 +12,21 @@ namespace Snake
         {
             Console.Title = "Snake";
 
-            int x1 = 1, x2 = 3, y1 = 6, y2 = 9;
-            char point1 = '*', point2 = '#';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 4;
+            p1.point = '*';
 
-            Cor(x1,y1,point1);
-            Cor(x2,y2,point2);
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 6;
+            p2.y = 8;
+            p2.point = '#';
+
+            p2.Draw();
 
             Console.ReadKey();
-        }
-
-        static void Cor(int x, int y, char point)
-        {
-            Console.SetCursorPosition( x, y );
-            Console.WriteLine(point);
         }
     }
 }
